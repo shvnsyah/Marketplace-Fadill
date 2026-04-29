7752aa2b21a1785d4b5f0fc86953ba04ea765813
shvnsyah <rifanfadillah724@gmail.com>
2026-04-29 19:18:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,268 @@
+    // ======================
+    // DATA PRODUK
+    // ======================
+    const produk = [
+    {
+    nama: "PC Rakitan",
+    harga: "Rp2.091.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.17.29.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAstMbHmSh-ndf0n/",
+    kategori: "pc"
+    },
+    {
+    nama: "PC Gaming & Streaming",
+    harga: "Rp5.714.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.20.04.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAsop35P6H-mZrzP/",
+    kategori: "pc"
+    },
+    {
+    nama: " Mouse Gaming Wireless",
+    harga: "Rp59.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.21.48.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAGhwBoJAg-GZjb6/",
+    kategori: "pc"
+    },
+    {
+    nama: "Mouse Office & Gaming",
+    harga: "Rp22.400",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.27.25.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAtAUEBw9a-IrGdo/",
+    kategori: "pc"
+    },
+    {
+    nama: "Mouse Gaming SKY",
+    harga: "Rp41.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.29.17.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAtmgGtMTm-ekSyB/",
+    kategori: "pc"
+    },
+    {
+    nama: "Mouse & Kyboard Logitech",
+    harga: "Rp149.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.31.11.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAtTgJCEBe-NvVJy/",
+    kategori: "pc"
+    },
+    {
+    nama: "Mouse & Kyboard Wireless MK215",
+    harga: "Rp226.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.32.40.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAnjjbkk3W-Zym3b/",
+    kategori: "pc"
+    },
+    {
+    nama: "Kyboard Mechanical ZIFRIEND KA646",
+    harga: "Rp184.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.33.47.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAnj5EMSkX-tR8oF/",
+    kategori: "pc"
+    },
+    {
+    nama: "Kyboard Mechanical ZIFRIEND T62 63",
+    harga: "Rp184.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.35.12.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAn4vVYW6T-aNJRf/",
+    kategori: "pc"
+    },
+    {
+    nama: "GAMEN GK300 Mechanical",
+    harga: "Rp141.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.36.12.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAnsg5X8pQ-TpP31/",
+    kategori: "pc"
+    },
+    {
+    nama: "SOUNDCORE R50I",
+    harga: "Rp201.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.37.31.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAnEWoGyNj-Em2Yo/",
+    kategori: "pc"
+    },
+    {
+    nama: "SOUNDCORE R50I NC",
+    harga: "Rp262.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.38.40.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAWkEdmUGE-5FMhL/",
+    kategori: "pc"
+    },
+    {
+    nama: "Rexus Vonix SH40",
+    harga: "Rp74.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.40.01.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAWamnsxjq-3piub/",
+    kategori: "pc"
+    },
+    {
+    nama: "Rexus Gaming Vonix",
+    harga: "Rp157.000",
+    gambar: "asset/WhatsApp Image 2026-04-26 at 00.41.34.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NAWuxxYVPu-8lFyf/",
+    kategori: "pc"
+    },
+    // bagian Fashion //
+    {
+    nama: "Cargo Pants",
+    harga: "Rp100.000",
+    gambar: "asset/WhatsApp Image 2026-04-24 at 16.19.58.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NM7ouhFfbF-vGMc8/",
+    kategori: "fashion"
+    },
+    {
+    nama: "Celana Pendek 3 pcs",
+    harga: "Rp65.000",
+    gambar: "asset/WhatsApp Image 2026-04-24 at 16.21.18.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NM7EM9gVQd-LkGP2/",
+    kategori: "fashion"
+    },
+    {
+    nama: "Short Pants 3 pcs",
+    harga: "Rp95.000",
+    gambar: "asset/WhatsApp Image 2026-04-24 at 16.24.10.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NMvDVmaXpR-kVdDo/",
+    kategori: "fashion"
+    },
+    {
+      nama: "Crgo short pants",
+    harga: "Rp58.000",
+    gambar: "asset/WhatsApp Image 2026-04-25 at 00.10.55.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NhkVRdhsN6-o8Gtt/",
+    kategori: "fashion"
+    },
+    {
+    nama: "Hoodie Boxy",
+    harga: "Rp67.000",
+    gambar: "asset/WhatsApp Image 2026-04-25 at 01.54.33.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NhXeRktq5Y-TJm7M/",
+    kategori: "fashion"
+    },
+    {
+      nama: "Double Zipper Hoodie Boxy",
+    harga: "Rp50.000",
+    gambar: "asset/WhatsApp Image 2026-04-25 at 01.56.12.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NhXHvfoDCe-p6T10/",
+    kategori: "fashion"
+    },
+    {
+      nama: "SWEATSHIRT HOODIE",
+    harga: "Rp83.000",
+    gambar: "asset/WhatsApp Image 2026-04-25 at 01.57.34.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9NhX3evXQSG-qz36u/",
+    kategori: "fashion"
+    },
+    {
+      nama: "TM OBSESSED",
+    harga: "Rp240.000",
+    gambar: "asset/WhatsApp Image 2026-04-25 at 01.59.09.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9Nh4JoMYwyH-RHhY5/",
+    kategori: "fashion"
+    },
+    {
+      nama: "Jaket Pria Waterproof DISAI",
+    harga: "Rp126.000",
+    gambar: "asset/WhatsApp Image 2026-04-25 at 02.00.17.jpeg",
+    link: "https://vt.tokopedia.com/t/ZS9Nh4juRMCvU-NaZcM/",
+    kategori: "fashion"
+    },
+
+    {
+      nama: "Glad2Glow Perfect Cover Waterproof Cushion",
+      harga: "Rp90.000",
+      gambar: "asset/WhatsApp Image 2026-04-29 at 17.28.25.jpeg",
+      link: "https://vt.tokopedia.com/t/ZS9NVjquQQvbT-D8zYm/",
+      kategori: "makeup"
+    },
+    {
+      nama: "Glad2Glow Blueberry Ceramide Low ph Cleanser",
+      harga: "Rp37.000",
+      gambar: "asset/WhatsApp Image 2026-04-29 at 17.29.30.jpeg",
+      link: "https://vt.tokopedia.com/t/ZS9NV6ackyDfV-IHAaQ/",
+      kategori: "makeup"
+    },
+    {
+      nama: "Glad2Glow Moisturizer Gethersuitable for all skin",
+      harga: "Rp43.000",
+      gambar: "asset/WhatsApp Image 2026-04-29 at 18.10.19.jpeg",
+      link: "https://vt.tokopedia.com/t/ZS9NVAXVx5p55-4cZLk/",
+      kategori: "makeup"
+    },
+    {
+      nama: "Khaf Face wash Triple action Oil and Commedo 100 ml",
+      harga: "Rp40.000",
+      gambar: "asset/WhatsApp Image 2026-04-29 at 18.13.13.jpeg",
+      link: "https://vt.tokopedia.com/t/ZS9NVAnNTreYa-rjbzk/",
+      kategori: "makeup"
+    },
+    {
+      nama: "Khaf Face Wash Oil and Acne 100 ml",
+      harga: "Rp40.000",
+      gambar: "asset/WhatsApp Image 2026-04-29 at 18.14.42.jpeg",
+      link: "https://vt.tokopedia.com/t/ZS9NVDF55SHvh-f0FTh/",
+      kategori: "makeup"
+    },
+    {
+      nama: "Khaf Face Wash Skin Energizing And Brightening 100 ml",
+      harga: "Rp40.000",
+      gambar: "asset/WhatsApp Image 2026-04-29 at 18.15.38.jpeg",
+      link: "https://vt.tokopedia.com/t/ZS9NVDftc1Mkc-17xNt/",
+      kategori: "makeup"
+    },
+    
+    
+    ];
+
+    // ======================
+    // RENDER PRODUK KE SLIDE
+    // ======================
+    produk.forEach(item => {
+
+    const card = document.createElement("div");
+    card.classList.add("produk");
+
+    card.innerHTML = `
+    <img src="${item.gambar}">
+    <div class="produk-info">
+      <h3>${item.nama}</h3>
+      <p class="harga">${item.harga}</p>
+      <small class="note">*Harga bisa berubah sewaktu-waktu</small>
+      <a href="${item.link}" class="btn" target="_blank">Beli Sekarang</a>
+    </div>
+    `;
+
+    if (item.kategori === "pc") {
+      document.getElementById("produk-slide1").appendChild(card);
+    } 
+    else if (item.kategori === "fashion") {
+      document.getElementById("produk-slide2").appendChild(card);
+    }
+    else if (item.kategori === "makeup") {
+      document.getElementById("produk-slide3").appendChild(card);
+    }
+
+    });
+
+
+    // ======================
+    // SLIDER
+    // ======================
+    let currentSlide = 0;
+
+    const slides = document.querySelectorAll(".slide");
+    const slideContainer = document.querySelector(".slide-container");
+
+    document.querySelector(".next").onclick = () => {
+      currentSlide = (currentSlide + 1) % slides.length;
+      updateSlide();
+    };
+
+    document.querySelector(".prev").onclick = () => {
+      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+      updateSlide();
+    };
+
+    function updateSlide() {
+      slideContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+    }
+
+
+    
